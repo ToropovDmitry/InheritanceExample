81a9f5404cff080c875a06053cc61b8faa86e683
Toropov Dmitry <dimonsladkoed@mail.ru>
2020-02-24 19:29:46 +0300

    Task 2

--- InheritanceExample.cpp
+++ InheritanceExample.cpp
@@ -6,124 +6,212 @@
 #include <string>
 #include <clocale>
 
-class creature
-{
-private:
-	std::string title;
-	double mass;
-protected:
-	creature() : mass(0.0) {}
-	creature(const creature& obj) : title(obj.title), mass(obj.mass) {}
-	creature(const std::string& _title, double _mass) : title(_title), mass(_mass) {}	
-	virtual ~creature() { std::cout << "creature deleted" << std::endl << std::endl; }	
-	virtual void _print() const { std::cout << "title: " << title << ", mass: " << mass; }	
-public:
-	void print() const
-	{
-		std::cout << typeid(*this).name() << ": (";		
-		_print();
-		std::cout << ")" << std::endl;
-	}
-	double get_mass() const 
-	{
-		return mass;
-	}
-};
-
-class animal : public creature
-{
-private:
-	double speed;
-public:
-	animal() : creature() {}
-	animal(const animal& obj) : creature(obj), speed(obj.speed) {}
-	animal(const std::string& _title, double _mass, double _speed)
-		: creature(_title, _mass), speed(_speed) {}
-	~animal() { std::cout << "animal deleted" << std::endl; }
-	double get_speed() const
-	{
-		return speed;
-	}
-protected:	
-	void _print() const
-	{
-		creature::_print();
-		std::cout << ", speed: " << speed;
-	}
-	
-};
-
-class bird : virtual public animal
-{
-private:
-	double topfly; 
-public:
-	bird() : animal() {}
-	bird(const bird& obj) : animal(obj), topfly(obj.topfly) {}
-	bird(const std::string& _title, double _mass, double _speed, double _topfly)
-		: animal(_title, _mass, _speed), topfly(_topfly) {}
-	~bird() { std::cout << "bird deleted" << std::endl; }
-protected:
-	void _print() const
-	{
-		animal::_print();
-		std::cout << ", topfly: " << topfly;
-	}
-};
-
-class fish : public animal
-{
-private:
-	double maxdeep;
-public:
-	fish() : animal() {}
-	fish(const fish& obj) : animal(obj), maxdeep(obj.maxdeep) {}
-	fish(const std::string& _title, double _mass, double _speed, double _maxdeep)
-		: animal(_title, _mass, _speed), maxdeep(_maxdeep) {}
-	~fish() { std::cout << "fish deleted" << std::endl; }
-protected:	
-	void _print() const
-	{
-		animal::_print();
-		std::cout << ", maxdeep: " << maxdeep;
-	}
-};
-
-class predator : virtual public animal 
+class ComputerGameObject
 {
 protected:
-	predator() {}
+	std::string name;	
+	int mana;	
+	int x;
+	int y;
 public:
-	~predator() {}
-	virtual bool hunt(const animal& obj) = 0;
+	virtual void draw() = 0;
 };
 
-class eagle : public bird, public predator 
+class Creature : public ComputerGameObject
+{
+protected:
+	int health;
+	int stamina;
+public:
+	virtual void move(int dx, int dy) = 0;
+};
+
+class MythologicalAnimal : public Creature
 {
 public:
-	eagle() : bird() {}
-	eagle(const eagle& obj) 
-		: bird(obj),
-		animal(obj) {}
-	eagle(double _mass, double _speed, double _topfly)
-		: bird("", 0, 0, _topfly),
-		animal("Орёл", _mass, _speed) {}
-	bool hunt(const animal& obj)
+	MythologicalAnimal() 
 	{
-		return obj.get_mass() < bird::get_mass() && obj.get_speed() < bird::get_speed();
+		this->name = "Мифическое животное";
+		this->health = 100;
+		this->mana = 100;
+		this->stamina = 100;
+		this->x = 0;
+		this->y = 0;
 	}
+	MythologicalAnimal(const MythologicalAnimal& mythologicalAnimal) 
+	{
+		this->name = mythologicalAnimal.name;
+		this->health = mythologicalAnimal.health;
+		this->mana = mythologicalAnimal.mana;
+		this->stamina = mythologicalAnimal.stamina;
+		this->x = mythologicalAnimal.x;
+		this->y = mythologicalAnimal.y;
+	}
+	MythologicalAnimal(std::string name, int heath, int mana, int stamina, int x, int y)
+	{
+		this->name = name;
+		this->health = heath;
+		this->mana = mana;
+		this->stamina = stamina;
+		this->x = x;
+		this->y = y;
+	}
+	void move(int dx, int dy)
+	{
+		this->x += dx;
+		this->y += dy;
+		if (dx != 0 || dy != 0)
+			std::cout << this->name << " движется в точку с координатами (" << this->x << ";" << this->y << ")" << std::endl;
+		else
+			std::cout << this->name << " стоит на месте, не обращая на вас внимания" << std::endl;
+	}
+	void draw()
+	{
+		std::cout << this->name << " стоит в точке с координатами (" << this->x << ";" << this->y << ")" << std::endl;
+	}
+	~MythologicalAnimal() {}
+};
+
+class Human : public Creature
+{
+public:
+	Human()
+	{
+		this->name = "Человек";
+		this->health = 100;
+		this->mana = 0;
+		this->stamina = 100;
+		this->x = 0;
+		this->y = 0;
+	}
+	Human(const Human& human)
+	{
+		this->name = human.name;
+		this->health = human.health;
+		this->mana = human.mana;
+		this->stamina = human.stamina;
+		this->x = human.x;
+		this->y = human.y;
+	}
+	Human(std::string name, int heath, int mana, int stamina, int x, int y)
+	{
+		this->name = name;
+		this->health = heath;
+		this->mana = mana;
+		this->stamina = stamina;
+		this->x = x;
+		this->y = y;
+	}
+	void move(int dx, int dy)
+	{
+		this->x += dx;
+		this->y += dy;
+		if (dx != 0 || dy != 0)
+			std::cout << this->name << " движется в точку с координатами (" << this->x << ";" << this->y << ")" << std::endl;
+		else
+			std::cout << this->name << " стоит на месте, не обращая на вас внимания" << std::endl;
+	}
+	void draw()
+	{
+		std::cout << this->name << " стоит в точке с координатами (" << this->x << ";" << this->y << ")" << std::endl;
+	}
+	~Human() {}
+};
+
+class MagicPerson : public Creature
+{
+public:
+	MagicPerson()
+	{
+		this->name = "Волшебное создание";
+		this->health = 100;
+		this->mana = 100;
+		this->stamina = 100;
+		this->x = 0;
+		this->y = 0;
+	}
+	MagicPerson(const MagicPerson& magicPerson)
+	{
+		this->name = magicPerson.name;
+		this->health = magicPerson.health;
+		this->mana = magicPerson.mana;
+		this->stamina = magicPerson.stamina;
+		this->x = magicPerson.x;
+		this->y = magicPerson.y;
+	}
+	MagicPerson(std::string name, int heath, int mana, int stamina, int x, int y)
+	{
+		this->name = name;
+		this->health = heath;
+		this->mana = mana;
+		this->stamina = stamina;
+		this->x = x;
+		this->y = y;
+	}
+	void move(int dx, int dy)
+	{
+		this->x += dx;
+		this->y += dy;
+		if (dx != 0 || dy != 0)
+			std::cout << this->name << " движется в точку с координатами (" << this->x << ";" << this->y << ")" << std::endl;
+		else
+			std::cout << this->name << " стоит на месте, не обращая на вас внимания" << std::endl;
+	}
+	void draw()
+	{
+		std::cout << this->name << " стоит в точке с координатами (" << this->x << ";" << this->y << ")" << std::endl;
+	}
+	~MagicPerson() {}
+};
+
+class MagicThing : public ComputerGameObject
+{
+public:
+	MagicThing()
+	{
+		this->name = "Волшебный предмет";		
+		this->mana = 100;		
+		this->x = 0;
+		this->y = 0;
+	}
+	MagicThing(const MagicThing& magicThing)
+	{
+		this->name = magicThing.name;		
+		this->mana = magicThing.mana;		
+		this->x = magicThing.x;
+		this->y = magicThing.y;
+	}
+	MagicThing(std::string name, int mana, int x, int y)
+	{
+		this->name = name;		
+		this->mana = mana;		
+		this->x = x;
+		this->y = y;
+	}	
+	void draw()
+	{
+		std::cout << this->name << " находиться в точке с координатами (" << this->x << ";" << this->y << ")" << std::endl;
+	}
+	~MagicThing() {}
 };
 
 int main()
 {
 	setlocale(0, "Rus");
-	bird raven("Ворона", 0.3, 10, 0.1);
-	eagle eagle(1, 100, 1);
-	fish hammerhead("Рыба-молот", 150, 5, 0.5);
-	raven.print();
-	hammerhead.print();
-	std::cout << "Eagle vs raven: " << eagle.hunt(raven) << std::endl;
-	std::cout << "Eagle vs hammerhead: " << eagle.hunt(hammerhead) << std::endl;
+	MythologicalAnimal pegasus = MythologicalAnimal("Пегас", 100, 100, 100, 5, 10);
+	Human perseus = Human("Персей", 100, 50, 100, 5, 10);
+	MagicPerson gorgon = MagicPerson("Медуза Горгона", 100, 100, 100, -11, 0);
+	MagicThing teardrop = MagicThing("Слеза Богов", 1000, -30, -30);
+	teardrop.draw();
+	pegasus.draw();
+	perseus.draw();
+	gorgon.draw();
+	pegasus.move(-10, 0);
+	perseus.move(-10, 0);
+	gorgon.move(-2, -5);
+	pegasus.draw();
+	perseus.draw();
+	gorgon.draw();
 	std::cout << std::endl;
 	return 0;
 }
