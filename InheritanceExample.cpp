5f341a52320add5e3db9660ec906754b84a882a8
Toropov Dmitry <dimonsladkoed@mail.ru>
2020-02-24 17:11:04 +0300

    Task1

--- InheritanceExample.cpp
+++ InheritanceExample.cpp
@@ -4,59 +4,59 @@
 #include "stdafx.h"
 #include <iostream>
 
-class B {
+class B1 {
 	int a;
 public:
-	B() { std::cout << "object of" << typeid(*this).name() << " was created\n"; }
-	B(int x) :B() { a = x; }
-	~B() { std::cout << "object of" << typeid(*this).name() << " was destroyed\n"; }
+	B1() { std::cout << "object of " << typeid(*this).name() << " was created\n"; }
+	B1(int x) :B1() { a = x; }
+	~B1() { std::cout << "object of " << typeid(*this).name() << " was destroyed\n"; }
 };
-class D1 : virtual public B {
+
+class B2 {
 	int b;
 public:
-	D1(int x, int y) : B(y)
-	{
-		b = x;
-		std::cout << "object of" << typeid(*this).name() << " was created\n";
-	};
-	~D1() { std::cout << "object of" << typeid(*this).name() << " was destroyed\n"; }
+	B2() { std::cout << "object of " << typeid(*this).name() << " was created\n"; }
+	B2(int x) :B2() { b = x; }
+	~B2() { std::cout << "object of " << typeid(*this).name() << " was destroyed\n"; }
 };
-class D2 : virtual private B {
+
+class D1 : private B1, public B2 {
 	int c;
 public:
-	D2(int x, int y) : B(y)
+	D1(int x, int y, int z) : B1(y), B2(z)
 	{
 		c = x;
-		std::cout << "object of" << typeid(*this).name() << " was created\n";
+		std::cout << "object of " << typeid(*this).name() << " was created\n";
 	};
-	~D2() { std::cout << "object of" << typeid(*this).name() << " was destroyed\n"; }
+	~D1() { std::cout << "object of " << typeid(*this).name() << " was destroyed\n"; }
 };
-class D3 : private D1, public D2 {
+
+class D2 : public B1, private B2 {
 	int d;
 public:
 
-	D3(int x, int y, int z, int i, int j) : D1(y, z), D2(i, j)
+	D2(int x, int y, int z) : B1(y), B2(z)
 	{
 		d = x;
-		std::cout << "object of" << typeid(*this).name() << " was created\n";
+		std::cout << "object of " << typeid(*this).name() << " was created\n";
 	}
-	~D3() { std::cout << "object of" << typeid(*this).name() << " was destroyed\n"; }
+	~D2() { std::cout << "object of " << typeid(*this).name() << " was destroyed\n"; }
 };
-class D4 : public D2, private D1 {
+
+class D3 : private D1, public D2 {
 	int e;
 public:
-	D4(int x, int y, int z, int i, int j) : D1(y, z), D2(i, j)
+	D3(int x, int y, int z, int i, int j, int k, int l) : D1(y, z, i), D2(j, k, l)
 	{
 		e = x;
-		std::cout << "object of" << typeid(*this).name() << " was created\n";
+		std::cout << "object of " << typeid(*this).name() << " was created\n";
 	}
-	~D4() { std::cout << "object of" << typeid(*this).name() << " was destroyed\n"; }
+	~D3() { std::cout << "object of " << typeid(*this).name() << " was destroyed\n"; }
 };
 
 int main()
 {
-	D3 temp(100, 200, 300, 400, 500);
-	D4 temp1(1, 2, 3, 4, 5);
+	D3 temp(1, 2, 3, 4, 5, 6, 7);
     return 0;
 }
 
